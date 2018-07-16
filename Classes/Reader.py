f1f92ad0995f18e4038436eb8468171114a73452
Justin Zhang <zhang.just@husky.neu.edu>
2018-07-16 14:52:37 -0400

    Able to check if a row is simple, reformated the csv so there are much fewer $ signs

--- Classes/Reader.py
+++ Classes/Reader.py
@@ -18,22 +18,35 @@ class Reader(ReaderAPI.ReaderAPI):
         return self.limit
 
     def _read(self):
-
         with open(self.getFileName()) as file:
             sheet = csv.reader(file, delimiter=',')
             self._skipHeader(sheet)
+            i = 0
             for row in sheet:
+                i += 1
                 self._process(row)
                 print(row)
+                if i >= self.getLimit():
+                    break
 
     # Skips the first row of the given csv
     def _skipHeader(self, sheet):
         next(sheet)
 
     def _process(self, row):
-        print #TODO
+        if self._simpleRow(row):
+            print # todo
+        else: #ignore the base cash flow
+            print # todo
 
-reader1 = Reader("../TestingWorkbook.csv", 5);
+    def _simpleRow(self, row):
+        # If row[2] is has no value or if the other columns(Expenses, ROC, Dist. Sub. to Recall, Income) are all empty
+        if (row[2] == '$-' or row[2] == "" or (row[3] == "" and row[4] == "" and row[5] == "" and row[6] == "")):
+            return True
+
+
+
+reader1 = Reader("../cbaCashFlowModel.csv", 5);
 
 
 
