a0038f759bd11a47a6ed938a1dfd20401fcc46e7
Justin Zhang <zhang.just@husky.neu.edu>
2018-07-19 16:18:05 -0400

    Added files to start a controller with some of the skeleton interface

---
+++ APIs/ControllerAPI.py
@@ -0,0 +1,14 @@
+class ControllerAPI(object):
+    # Signatures not yet compelete
+    # Simple Functions
+    def getFundTransactions(self, fund):
+        return NotImplementedError("Abstract Class")
+
+    # Calculated Functions
+    # Possible use of higher order functions here?
+    def remainingCommitment(self, fundID):
+        return NotImplementedError("Abstract Class")
+    def currentNAV(self, fundID):
+        return NotImplementedError("Abstract Class")
+    def getGrowth(self, fundID):
+        return NotImplementedError("Abstract Class")
