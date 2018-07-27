f95534758ceef9c07ab6e627de9992f322635daa
Justin Zhang <zhang.just@husky.neu.edu>
2018-07-27 10:58:23 -0400

    made sure to add the stored function to the mysql RawDateTableCreation save file

--- RawDataTableCreation.sql
+++ RawDataTableCreation.sql
@@ -92,6 +92,41 @@ LOCK TABLES `Fund` WRITE;
 /*!40000 ALTER TABLE `Fund` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Fund` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cbaDB'
+--
+/*!50003 DROP FUNCTION IF EXISTS `remainingCommitment` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `remainingCommitment`(fund VARCHAR(255), endDate DATE) RETURNS int(11)
+    DETERMINISTIC
+BEGIN
+		RETURN (SELECT SUM(CashFlow.cashValue) 
+				FROM CashFlow INNER JOIN CashFlowType ON(CashFlow.typeID = CashFlowType.typeID)
+                WHERE CashFlow.fundID = fund AND 
+					CashFlow.cfDate <= endDate AND 
+                    (CashFlowType.useCase = 'Investment' OR 
+					CashFlowType.useCase = 'Subject to Recall' OR
+                    CashFlowType.useCase = 'Initial Commitment' OR
+                    (CashFlow.cashValue < 0 AND 
+						(CashFlowType.result = 'Distribution' OR
+						CashFLowType.result = 'Contribution')
+                        )));
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -102,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-27 10:42:00
+-- Dump completed on 2018-07-27 10:57:17
